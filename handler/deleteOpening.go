3c4fa06d0ae07b2a6d2606a0a60ebd4ab80a6f3e
Halan Germano Bacca <halanbacca@outlook.com>
2026-01-03 00:44:23 -0300

    Adiciona operação DELETE

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -4,10 +4,26 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/halangbacca/gopportunities/schemas"
 )
 
-func DeleteOpeningHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "DELETE Opening",
-	})
+func DeleteOpeningHandler(ctx *gin.Context) {
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, err.Error())
+		return
+	}
+
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendSuccess(ctx, "deleteOpening", opening)
 }
